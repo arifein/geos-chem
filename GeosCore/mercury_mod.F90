e93d4200f9d3c3fa254a0710ecd038cc09723063
Eric Roy <ericmroy00@gmail.com>
2025-02-12 13:35:13 -0500

    Added requirement for ocean concentrations to be greater than zero for ocean emission calculations

--- GeosCore/mercury_mod.F90
+++ GeosCore/mercury_mod.F90
@@ -3368,8 +3368,9 @@ CONTAINS
        FRAC_L       = State_Met%FRLAND(I,J)
        FRAC_O       = MAX( State_Met%FROCEAN(I,J) - &
                            State_Met%FRSEAICE(I,J), 0.0_fp )
-       IS_OCEAN_BOX = ( ( FRAC_O > 0.0_fp ) .and. &
-                        ( State_Met%SEAICE00(I,J)  > 0.5_fp ) )
+       IS_OCEAN_BOX = ( ( FRAC_O > 0.0_fp )                   .and. &
+                        ( State_Met%SEAICE00(I,J)  > 0.5_fp ) .and. &
+                        ( Hg0aq(I,J) > 0.0_fp ) )                     ! emr 02/12/25 skipping box if Hg0aq is zero
 
        IF ( IS_OCEAN_BOX ) THEN
 
@@ -3509,6 +3510,13 @@ CONTAINS
           !--------------------------------------------------------
           ! Flux limited by ocean and atm Hg(0)
           !--------------------------------------------------------
+
+          ! emr 2/12/25 replacing flux with 0 if CHg0aq is zero
+          IF ( Hg0aq(I,J) = 0.0_fp ) THEN
+             FLUX(I,J)  = 0.0_fp
+             FUP(I,J)   = 0.0_fp
+             FDOWN(I,J) = 0.0_fp
+          ENDIF
 
           ! Cap the flux w/ the available Hg(0) ocean mass
           Hg0aqtemp = CHg0aq * A_M2 * FRAC_O *1.0e-8_fp
